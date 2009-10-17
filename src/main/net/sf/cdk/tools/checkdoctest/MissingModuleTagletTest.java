76d2c060f491ebc1a7bdb038765e70eff8462eac
Egon Willighagen <egonw@users.sourceforge.net>
2009-10-17 22:28:18 +0200

    Added OpenJavaDocCheck library (new BSD licensed) and written a custom JavaDoc check to test of the cdk.module tag has been given in the class JavaDoc.

---
+++ src/main/net/sf/cdk/tools/checkdoctest/MissingModuleTagletTest.java
@@ -0,0 +1,57 @@
+/* Copyright (C) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package net.sf.cdk.tools.checkdoctest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.ojdcheck.test.IClassDocTester;
+import com.github.ojdcheck.test.ITestReport;
+import com.github.ojdcheck.test.TestReport;
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.Tag;
+
+public class MissingModuleTagletTest implements IClassDocTester {
+
+    public String getDescription() {
+        return "Tests the content of the module taglet content.";
+    }
+
+    public String getName() {
+        return "CDK Module Taglet Test";
+    }
+
+    public List<ITestReport> test(ClassDoc classDoc) {
+        List<ITestReport> reports = new ArrayList<ITestReport>();
+        Tag[] tags = classDoc.tags("cdk.module");
+        if (tags.length == 0) {
+            reports.add(
+                new TestReport(
+                    this, classDoc,
+                    "Missing @cdk.module tag.",
+                    classDoc.position().line(), null
+                )
+            );
+        }
+        return reports;
+    }
+
+    public Priority getPriority() {
+        return Priority.WARNING;
+    }
+
+}
